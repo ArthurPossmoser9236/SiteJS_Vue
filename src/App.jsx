ee1a87d6aaed53b8fb83242bf76052391c0e050e
Arthur Possmoser <arthurPossmoser.TI2024@gmail.com>
2025-03-28 11:57:23 -0300

    Adicionando todos os arquivos do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Header from './componentes/Header'
 import './App.css'
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
 
   return (
     <>
+
+    <Header />
       <div className="page">
         <div className="title">
           <h1>Título</h1>
---
+++ src/assets/componentes/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function header(){
+    return (
+        <>
+        
+        <header>teste</header> 
+
+        </>
+    )
+}
